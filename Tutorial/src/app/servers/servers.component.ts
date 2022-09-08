474fba5df95a7d89fd9144dddf5d708ef0e4ad2b
Josef Sebastian Duschl <josef-sebastian.Duschl@adesso.de>
2022-09-08 09:37:31 +0200

    add 31 Bindalbe Properties & Events

--- Tutorial/src/app/servers/servers.component.ts
+++ Tutorial/src/app/servers/servers.component.ts
@@ -15,6 +15,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = 'No server was created';
 
   constructor() {
     setTimeout(() => {
@@ -24,6 +25,10 @@ export class ServersComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was Created!';
   }
 
 }
