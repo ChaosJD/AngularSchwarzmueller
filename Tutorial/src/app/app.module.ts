4a15f8554f012c93f7e51a81cc627d84a1130b16
Josef Sebastian Duschl <josef-sebastian.Duschl@adesso.de>
2022-09-09 11:25:45 +0200

    completed The Basics only the course

--- Tutorial/src/app/app.module.ts
+++ Tutorial/src/app/app.module.ts
@@ -3,18 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {ServerComponent} from "./server/server.component";
-import { ServersComponent } from './servers/servers.component';
+import {ServersComponent} from './servers/servers.component';
+import {FormsModule} from "@angular/forms";
 //import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
   ],
   imports: [
     BrowserModule,
-    //FormsModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Tutorial/src/app/servers/servers.component.ts
+++ Tutorial/src/app/servers/servers.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, NgModule, OnInit} from '@angular/core';
+import {Event} from "@angular/router";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   //selector: '[app-servers]', // to select elements use []. Now it is an attribute!
@@ -10,12 +12,15 @@ import { Component, OnInit } from '@angular/core';
   //   <app-server></app-server>
   // `,
   // template(local)or templateUrl(external) must be set with ' or backtickt:` to use javascript template expression over mor lines, NO Array!
-  templateUrl: '/servers.component.html',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
+  serverName = 'Testserver';
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver 2']
 
   constructor() {
     setTimeout(() => {
@@ -27,9 +32,14 @@ export class ServersComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onCreateServer(){
-    this.serverCreationStatus = 'Server was Created!';
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'Server was Created! Name ist ' + this.serverName;
   }
 
+  onUpdateServerName(event: any) { // before: event: any, New! devilation from Tutorial. Use InputEvent Instead of Event, it is old.
+    console.log(event);
+    this.serverName = event.target.value;
+  }
 }
-
